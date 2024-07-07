25e16eb439db5008e439e130b375dd14a0d207cf
Ryan Turner <ryanturner02@protonmail.com>
2024-07-07 18:11:52 -0400

    Update card border

--- src/app/components/cards/card.tsx
+++ src/app/components/cards/card.tsx
@@ -9,16 +9,16 @@ type CardInformation = {
 
 export default function Card({ title, releaseDate, description, technologies }: CardInformation) {
   return (
-    <div className="grid grid-rows-[auto,1fr,auto] gap-4 card-border border border-black rounded p-3 w-full h-full">
+    <div className="grid grid-rows-[auto,1fr,auto] gap-4 card-border border-2 border-gray-300 rounded-lg p-3 w-full h-full">
       <header className="card-header">
-        <h2 className="card-title text-xl font-semibold border-black">{title}</h2>
+        <h2 className="card-title text-xl font-semibold">{title}</h2>
         <div className="card-links">
         </div>
         {
           releaseDate && <p className="card-release-date text-sm">{releaseDate}</p>
         }
       </header>
-      <p className="card-description border-black pb-3">{description}</p>
+      <p className="card-description pb-4">{description}</p>
       <p className="card-technologies self-end text-xs">{technologies}</p>
     </div>
   );
