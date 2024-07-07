3dea3e8ade2cb76cc5f7aad4480d4cde91bd7d06
Ryan Turner <ryanturner02@protonmail.com>
2024-07-07 15:50:16 -0400

    Add heading for games and projects pages

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,9 @@ export default function Projects() {
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex justify-center mb-10">
-        <p>Projects</p>
+        <article className="sm:w-2/3 w-5/6">
+          <h1 className="text-3xl font-bold text-center">Projects</h1>
+        </article>
       </main>
     </div>
   );
