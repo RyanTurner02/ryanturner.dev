44fa9f4ca2f652352f2492c30e1dd85b1c218c4c
Ryan Turner <ryanturner02@protonmail.com>
2024-07-06 21:10:46 -0400

    Update alt text and display LinkedIn logo

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -4,9 +4,10 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center space-x-3">
-      <Image src="assets/github/github-mark-light.svg" alt="GitHub Light Logo" width={25} height={25} />
-      <Image src="assets/envelope/envelope-light.svg" alt="GitHub Light Logo" width={25} height={25} />
+    <footer className="flex justify-center space-x-3 mb-10">
+      <Image src="assets/github/github-mark-light.svg" alt="GitHub Logo" width={25} height={25} />
+      <Image src="assets/linkedin/linkedin-light.svg" alt="LinkedIn Logo" width={25} height={25} />
+      <Image src="assets/envelope/envelope-light.svg" alt="Email Logo" width={25} height={25} />
     </footer>
   );
 }
