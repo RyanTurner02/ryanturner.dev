58a589c18b2cad211026c72d4ff6f827c09b9189
Ryan Turner <ryanturner02@protonmail.com>
2024-07-06 21:31:05 -0400

    Add GitHub and LinkedIn links

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,12 +1,19 @@
 'use client'
 
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer className="flex justify-center space-x-3 mb-10">
-      <Image src="assets/github/github-mark-light.svg" alt="GitHub Logo" width={25} height={25} />
-      <Image src="assets/linkedin/linkedin-light.svg" alt="LinkedIn Logo" width={25} height={25} />
+      <Link href="https://github.com/RyanTurner02">
+        <Image src="assets/github/github-mark-light.svg" alt="GitHub Logo" width={25} height={25} />
+      </Link>
+
+      <Link href="https://www.linkedin.com/in/ryanturner02/">
+        <Image src="assets/linkedin/linkedin-light.svg" alt="LinkedIn Logo" width={25} height={25} />
+      </Link>
+
       <Image src="assets/envelope/envelope-light.svg" alt="Email Logo" width={25} height={25} />
     </footer>
   );
