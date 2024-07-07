fb8c7ecf3082bd5cded200d232f460dcbf7ee296
Ryan Turner <ryanturner02@protonmail.com>
2024-07-07 15:35:57 -0400

    Add styling from main page to project and games pages

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,11 +2,11 @@ import Header from "@/app/components/header/header";
 
 export default function Projects() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex justify-center">
+      <main className="flex justify-center mb-10">
         <p>Projects</p>
       </main>
-    </>
+    </div>
   );
 }
