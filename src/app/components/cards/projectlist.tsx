209e077db3116b7398a6aa389d4d17511bb33252
Ryan Turner <ryanturner02@protonmail.com>
2024-07-09 23:38:20 -0400

    Use 3 columns on large devices on projects page

--- src/app/components/cards/projectlist.tsx
+++ src/app/components/cards/projectlist.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function ProjectList() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <Card
         title="Pocketwatch"
         description="A MERN stack web application for tracking time spent on tasks."
