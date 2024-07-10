ca1ccdb59b7d1bf5ea2a41888afdbd4502990f6b
Ryan Turner <ryanturner02@protonmail.com>
2024-07-09 23:34:53 -0400

    Add projects and games to their own components

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,6 +1,6 @@
 import Header from "@/app/components/header/header";
 import Footer from "@/app/components/footer/footer";
-import Card from "@/app/components/cards/card";
+import GameList from "@/app/components/cards/gamelist";
 
 export default function Projects() {
   return (
@@ -9,20 +9,7 @@ export default function Projects() {
       <main className="flex justify-center mb-10">
         <article className="w-11/12 sm:w-4/5 md:2/3">
           <h1 className="text-3xl font-bold text-center mb-4">Games</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <Card
-              title="Space Havoc"
-              releaseDate="December 4, 2023"
-              description="A 3D Space Shooter inspired by old arcade and Atari games."
-              technologies="C++, raylib, raygui"
-            />
-            <Card
-              title="Volcanic Bird: Bird Quest"
-              releaseDate="December 1, 2023"
-              description="An interactive text-based creature-collecting dungeon crawler."
-              technologies="Godot Engine, GDScript"
-            />
-          </div>
+          <GameList />
         </article>
       </main>
       <Footer />
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,6 @@
 import Header from "@/app/components/header/header";
 import Footer from "@/app/components/footer/footer";
-import Card from "@/app/components/cards/card";
-import Link from "next/link";
+import ProjectList from "@/app/components/cards/projectlist";
 import "@/app/styles/links.css";
 
 export default function Projects() {
@@ -11,57 +10,7 @@ export default function Projects() {
       <main className="flex justify-center mb-10">
         <article className="w-11/12 sm:w-4/5 md:2/3">
           <h1 className="text-3xl font-bold text-center mb-4">Projects</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-            <Card
-              title="Pocketwatch"
-              description="A MERN stack web application for tracking time spent on tasks."
-              technologies="HTML, CSS, JavaScript, React, Bootstrap"
-            />
-            <Card
-              title="Contact Manager"
-              description="A LAMP stack web application for managing a list of contact information."
-              technologies="HTML, CSS, JavaScript, PHP, SQL, MySQL, Bootstrap"
-            />
-            <Card
-              title="KHInsider Downloader"
-              description={
-                <>
-                  A Java web scraper for downloading video game albums from <Link className="text-link" href="https://downloads.khinsider.com/">KHInsider</Link>.
-                </>
-              }
-              technologies="Java, JSoup, Apache Commons CLI, Apache Commons IO"
-            />
-            <Card
-              title="Inventory Manager"
-              description="A JavaFX desktop application for creating, modifying, searching, and deleting a list of inventory items."
-              technologies="Java, JavaFX, JUnit, Gson, JSoup"
-            />
-            <Card
-              title="ryanturner.dev"
-              description="My personal website."
-              technologies="HTML, CSS, TypeScript, React, Next.js, TailwindCSS"
-            />
-            <Card
-              title="1440"
-              description="A countdown timer for the remaining minutes of the day."
-              technologies="HTML, CSS, TypeScript, React, TailwindCSS"
-            />
-            <Card
-              title="Startpage"
-              description="A custom webpage for the browser on its startup."
-              technologies="HTML, CSS, JavaScript, Bootstrap"
-            />
-            <Card
-              title="Pomodoro Timer"
-              description="A simple and easy-to-use pomodoro timer."
-              technologies="HTML, CSS, JavaScript"
-            />
-            <Card
-              title="Wikipedia Philosophy"
-              description="A web crawler for getting to the Philosophy article on Wikipedia."
-              technologies="Java, JSoup"
-            />
-          </div>
+          <ProjectList />
         </article>
       </main>
       <Footer />
