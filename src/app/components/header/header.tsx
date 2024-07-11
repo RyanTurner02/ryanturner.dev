06476ea218f7487616cc05bcc4f78f77482a7439
Ryan Turner <ryanturner02@protonmail.com>
2024-07-11 19:42:01 -0400

    Add sidebar header

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import Navbar from "@/app/components/navigation/navbar";
-import Sidebar from "@/app/components/navigation/sidebar";
+import SidebarHeader from "@/app/components/navigation/sidebar-header";
 
 export default function Header() {
   return (
     <header className="mt-2 sm:mt-4 pb-2 sm:pb-4 mb-4 border-b">
-      <Sidebar />
+      <SidebarHeader />
       <Navbar />
     </header>
   );
---
+++ src/app/components/navigation/sidebar-header.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function SidebarHeader() {
+  const [isDisplaying, setIsDisplaying] = useState(false);
+  
+  const toggleSidebar = () => {
+    setIsDisplaying(!isDisplaying);
+  };
+
+  const hamburgerImage = <Image src="assets/hamburger/hamburger.svg" alt="" onClick={toggleSidebar} width={25} height={25} />;
+  const xmarkImage = <Image src="assets/xmark/xmark.svg" alt="" onClick={toggleSidebar} width={25} height={25} />;
+
+  const toggleSidebarImage = () => {
+    return isDisplaying ? xmarkImage : hamburgerImage;
+  };
+
+  return (
+    <div className="flex sm:hidden justify-between ml-4 mr-4">
+      <Link className="header-link" href="/">Home</Link>
+      { toggleSidebarImage() }
+    </div>
+  );
+}
