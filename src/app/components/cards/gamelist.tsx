476a2a9bfeb7fbc442c9352c6aecd7fc420c4d54
Ryan Turner <ryanturner02@protonmail.com>
2024-07-11 21:01:23 -0400

    Add game links

--- src/app/components/cards/gamelist.tsx
+++ src/app/components/cards/gamelist.tsx
@@ -7,12 +7,14 @@ export default function GameList() {
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
       <Card
         title="Space Havoc"
+        links={{ githubURL: "https://github.com/RyanTurner02/space-havoc" }}
         releaseDate="December 4, 2023"
         description="A 3D Space Shooter inspired by old arcade and Atari games."
         technologies="C++, raylib, raygui"
       />
       <Card
         title="Volcanic Bird: Bird Quest"
+        links={{ githubURL: "https://github.com/PKTidus/volcanic-bird-rpg", itchURL: "https://rockmandude.itch.io/vbbq" }}
         releaseDate="December 1, 2023"
         description="An interactive text-based creature-collecting dungeon crawler."
         technologies="Godot Engine, GDScript"
