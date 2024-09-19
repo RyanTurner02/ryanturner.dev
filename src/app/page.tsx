227798d7fbf8e5b808934b07d69ef0edca5fe8b3
Ryan Turner <ryanturner02@protonmail.com>
2024-09-18 20:20:20 -0400

    Update about me text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           <h1 className="text-3xl font-bold text-center mb-4">Welcome</h1>
           <section className="text-left sm:text-center">
             <p>I am an aspiring Software Engineer and a Computer Science student at the University of Central Florida.</p>
-            <p>You can find the <Link className="text-link" href="/projects">projects</Link> and <Link className="text-link" href="/games">games</Link> I worked on on my website.</p>
+            <p>You can find my <Link className="text-link" href="/projects">projects</Link> and <Link className="text-link" href="/games">games</Link> on my website.</p>
           </section>
         </article>
       </main>
